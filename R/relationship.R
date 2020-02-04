f7d9e482146a6d668ead4efdb7f8aef6499c9643
paolaserra <60321304+paolaserra@users.noreply.github.com>
2020-02-04 13:40:08 +0100

    fourth commit

--- R/relationship.R
+++ R/relationship.R
@@ -1,4 +1,15 @@
+#' This function shows the relatioship between some variables and accountbalance
+#'
+#'
+#' @param ~x is a name of a variable (Age, Income, Expenditure)
+#' @return the plot
+#' @export
+#' @example
+#' relationship(~Age)
+#'
+#
+#
 
-relatioship <-function(x) {
+relationship <-function(x) {
   plot_ly(accountbalance, x= (x), y = ~Accbl)
 }
