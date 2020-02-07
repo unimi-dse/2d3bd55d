6fdba106734360cf7344e41e718a84d9ec2cadee
paolaserra <60321304+paolaserra@users.noreply.github.com>
2020-02-07 17:01:48 +0100

    eight

--- R/plotres.R
+++ R/plotres.R
@@ -1,17 +1,15 @@
-#'  This function plots the residual against the predicted model
+#' plotres
+#' @description This function plots the residuals against the predicted model
 #'
-#'  @param x is/are the observed variable/s that we predict using predict
-#'  @param y are the residuals
-#'  @import graphics
-#'  @import stats
-#'  @export
-#'  @examples
-#'  plotres(predict(linear.r), residuals(linear.r))
-#'  plotres(predict(linearD), residuals(linearD))
+#' @param x is the dataset on which the function calculates the residual and the predicted value
+#' @export
+#' @import stats graphics
 #'
 #'
-plotres <- function(x,y) {
-  plot(predict(x), (y))
+#'
+#'
+plotres <- function(x) {
+  plot(predict(x), residuals(x))
 
   abline(a=0,b=0, col ='red')
 }
